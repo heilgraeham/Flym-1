72d0daa7f474528040a02ee63a6433840f3e05e5
Connor Powers <powers2537@gmail.com>
2017-11-22 11:32:03 -0500

    adding unit testing to master

--- Flym/src/main/java/net/fred/feedex/utils/NetworkUtils.java
+++ Flym/src/main/java/net/fred/feedex/utils/NetworkUtils.java
@@ -41,6 +41,7 @@ import java.io.InputStream;
 import java.net.CookieHandler;
 import java.net.CookieManager;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashSet;
 
@@ -166,14 +167,21 @@ public class NetworkUtils {
         return downloadPictures;
     }
 
-    public static String getBaseUrl(String link) {
-        String baseUrl = link;
+    public static String getBaseUrl(String link) throws MalformedURLException {
+
+
+        URL url = new URL(link);
+        String baseUrl =  url.getAuthority();
+        return baseUrl;
+
+
+        /*
         int index = link.indexOf('/', 8); // this also covers https://
         if (index > -1) {
             baseUrl = link.substring(0, index);
-        }
+        }*/
 
-        return baseUrl;
+
     }
 
     public static byte[] getBytes(InputStream inputStream) throws IOException {
