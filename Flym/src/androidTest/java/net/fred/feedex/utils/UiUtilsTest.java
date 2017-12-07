945fc9c35a845b611bad4565b65c3e55eda91a32
Eric Boychan <ericboych1@gmail.com>
2017-12-07 15:25:12 -0500

    added a unit test.

--- Flym/src/androidTest/java/net/fred/feedex/utils/UiUtilsTest.java
+++ Flym/src/androidTest/java/net/fred/feedex/utils/UiUtilsTest.java
@@ -1,7 +1,13 @@
 package net.fred.feedex.utils;
 
+import android.content.ContentResolver;
+import android.database.CharArrayBuffer;
+import android.database.ContentObserver;
 import android.database.Cursor;
+import android.database.DataSetObserver;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.os.Bundle;
 
 import net.fred.feedex.provider.DatabaseHelper;
 
@@ -17,6 +23,217 @@ public class UiUtilsTest {
     public void getScaledBitmap() throws Exception {
         byte[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
         assertEquals(null, UiUtils.getScaledBitmap(array, 20));
+
+    }
+
+    @Test
+    public void getFaviconBitmap() throws Exception {
+        assertEquals(null, UiUtils.getFaviconBitmap(12345678910L, new Cursor() {
+            @Override
+            public int getCount() {
+                return 0;
+            }
+
+            @Override
+            public int getPosition() {
+                return 0;
+            }
+
+            @Override
+            public boolean move(int i) {
+                return false;
+            }
+
+            @Override
+            public boolean moveToPosition(int i) {
+                return false;
+            }
+
+            @Override
+            public boolean moveToFirst() {
+                return false;
+            }
+
+            @Override
+            public boolean moveToLast() {
+                return false;
+            }
+
+            @Override
+            public boolean moveToNext() {
+                return false;
+            }
+
+            @Override
+            public boolean moveToPrevious() {
+                return false;
+            }
+
+            @Override
+            public boolean isFirst() {
+                return false;
+            }
+
+            @Override
+            public boolean isLast() {
+                return false;
+            }
+
+            @Override
+            public boolean isBeforeFirst() {
+                return false;
+            }
+
+            @Override
+            public boolean isAfterLast() {
+                return false;
+            }
+
+            @Override
+            public int getColumnIndex(String s) {
+                return 0;
+            }
+
+            @Override
+            public int getColumnIndexOrThrow(String s) throws IllegalArgumentException {
+                return 0;
+            }
+
+            @Override
+            public String getColumnName(int i) {
+                return null;
+            }
+
+            @Override
+            public String[] getColumnNames() {
+                return new String[0];
+            }
+
+            @Override
+            public int getColumnCount() {
+                return 0;
+            }
+
+            @Override
+            public byte[] getBlob(int i) {
+                return new byte[0];
+            }
+
+            @Override
+            public String getString(int i) {
+                return null;
+            }
+
+            @Override
+            public void copyStringToBuffer(int i, CharArrayBuffer charArrayBuffer) {
+
+            }
+
+            @Override
+            public short getShort(int i) {
+                return 0;
+            }
+
+            @Override
+            public int getInt(int i) {
+                return 0;
+            }
+
+            @Override
+            public long getLong(int i) {
+                return 0;
+            }
+
+            @Override
+            public float getFloat(int i) {
+                return 0;
+            }
+
+            @Override
+            public double getDouble(int i) {
+                return 0;
+            }
+
+            @Override
+            public int getType(int i) {
+                return 0;
+            }
+
+            @Override
+            public boolean isNull(int i) {
+                return false;
+            }
+
+            @Override
+            public void deactivate() {
+
+            }
+
+            @Override
+            public boolean requery() {
+                return false;
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public boolean isClosed() {
+                return false;
+            }
+
+            @Override
+            public void registerContentObserver(ContentObserver contentObserver) {
+
+            }
+
+            @Override
+            public void unregisterContentObserver(ContentObserver contentObserver) {
+
+            }
+
+            @Override
+            public void registerDataSetObserver(DataSetObserver dataSetObserver) {
+
+            }
+
+            @Override
+            public void unregisterDataSetObserver(DataSetObserver dataSetObserver) {
+
+            }
+
+            @Override
+            public void setNotificationUri(ContentResolver contentResolver, Uri uri) {
+
+            }
+
+            @Override
+            public Uri getNotificationUri() {
+                return null;
+            }
+
+            @Override
+            public boolean getWantsAllOnMoveCalls() {
+                return false;
+            }
+
+            @Override
+            public void setExtras(Bundle bundle) {
+
+            }
+
+            @Override
+            public Bundle getExtras() {
+                return null;
+            }
+
+            @Override
+            public Bundle respond(Bundle bundle) {
+                return null;
+            }
+        }, -1));
 
     }
 
